3013dc55fd25bc914e00f6e77529eeb2690d317c
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2018-11-14 22:01:10 +0200

    Print failed and succeeded test cases

--- run.php
+++ run.php
@@ -1,9 +1,11 @@
 #!/usr/bin/php
 <?php
 
+class AssertionFailed extends Exception {}
+
 function assertEqual($expect, $value, $message = null) {
   if ($expect !== $value) {
-    throw new Exception("values are not equal: ". $message);
+    throw new AssertionFailed("values are not equal: ". $message);
   }
 }
 
@@ -20,10 +22,21 @@ class SomeTestCase {
 
 class RunPHPRunner {
   public function run($obj) {
+    $this->log("Running tests...");
+    $this->log(sprintf("%s:", get_class($obj)));
     $methods = $this->getTestMethods($obj);
     foreach ($methods as $method) {
-      $obj->$method();
+      try {
+        $obj->$method();
+        $this->log(sprintf("[+] %s", $method));
+      } catch (AssertionFailed $e) {
+        $this->log(sprintf("[-] %s: %s", $method, $e->getMessage()));
+      }
     }
+  }
+
+  private function log($message) {
+    echo $message . PHP_EOL;
   }
 
   private function getTestMethods($obj) {
