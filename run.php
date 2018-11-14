9902f76b244a788af6d7c5875f6926a2a5c25736
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2018-11-14 21:53:42 +0200

    Extract test methods from testCase

--- run.php
+++ run.php
@@ -7,4 +7,37 @@ function assertEqual($expect, $value, $message = null) {
   }
 }
 
-assertEqual(true, false);
+class SomeTestCase {
+  public function testSomething() {
+    assertEqual(true, false);
+  }
+
+  public function testSomethingElse() {
+    assertEqual(1, 1);
+  }
+}
+
+
+class RunPHPRunner {
+  public function run($obj) {
+    $methods = $this->getTestMethods($obj);
+    foreach ($methods as $method) {
+      $obj->$method();
+    }
+  }
+
+  private function getTestMethods($obj) {
+    $methods = array_filter(
+      get_class_methods($obj),
+      function($method) {
+        return preg_match('/^test[A-Z0-9]/', $method);
+      }
+    );
+
+    return $methods;
+  }
+}
+
+$testCase = new SomeTestCase;
+$runner = new RunPHPRunner;
+$runner->run($testCase);
